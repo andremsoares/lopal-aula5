b201c0e5bf1b4de48a3126dbec21afbb8a709592
ANDRÉ MATHEUS SOARES DA SILVA <26210897@dsn127m1246733.cfp127aluno.local>
2026-09-18 13:31:47 -0300

    menu estruturado

--- 01-formas.py
+++ 01-formas.py
@@ -1,1 +1,13 @@
-print ("CALCULADORA DE ÁREA DE FORMAS GEOMÉTRICAS")
+while True:
+    print ("CALCULADORA DE ÁREAS DAS FORMAS GEOMÉTRICAS")
+    print ("1 - Círculo")
+    print ("2 - Triângulo")
+    print ("3 - Quadrado")
+    print ("4 - Retângulo")
+    print ("5 - Paralelogramo")
+    print ("6 - Losango")
+    print ("7 - Trapézio")
+    print ("0 - Sair do sistema")
+    
+    opcao = input("Escolha sua opção: ")
+    
