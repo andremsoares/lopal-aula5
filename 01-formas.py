2c55079f54175c3d4730443a1470afd48f95655c
ANDRÉ MATHEUS SOARES DA SILVA <26210897@dsn127m1246733.cfp127aluno.local>
2026-09-18 13:37:49 -0300

    função do cálculo círculo pronta

--- 01-formas.py
+++ 01-formas.py
@@ -1,3 +1,8 @@
+def circulo():
+    raio = float(input("Qual o valor do raio do círculo?"))
+    resultado = 3.14 * raio ** 2
+    print (f"A área do círculo é: {resultado}")
+
 while True:
     print ("CALCULADORA DE ÁREAS DAS FORMAS GEOMÉTRICAS")
     print ("1 - Círculo")
@@ -10,4 +15,7 @@ while True:
     print ("0 - Sair do sistema")
     
     opcao = input("Escolha sua opção: ")
+    
+    if opcao == "1":
+        circulo()
     
