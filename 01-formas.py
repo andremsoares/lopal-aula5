b1c7404988bc93be5ace3323a955178656baf711
ANDRÉ MATHEUS SOARES DA SILVA <26210897@dsn127m1246733.cfp127aluno.local>
2026-09-18 14:05:28 -0300

    função do cálculo da área do trapézio feita

--- 01-formas.py
+++ 01-formas.py
@@ -1,36 +1,43 @@
 def circulo():
     raio = float(input("Qual o valor do raio do círculo? "))
     resultado = 3.14 * raio ** 2
-    print (f"A área do seu círculo é: {resultado}")
+    print (f"A área do círculo é: {resultado}")
     
 def triangulo():
     base = float(input("Qual o valor da base do triângulo? "))
     altura = float(input("Qual o valor da altura do triângulo? "))
     resultado = (base * altura) / 2
-    print (f"A área do seu triângulo é {resultado}")
+    print (f"A área do triângulo é {resultado}")
     
 def quadrado():
     lado = float(input("Qual o valor do lado do quadrado? "))
     resultado = lado ** 2
-    print (f"A área do seu quadrado é {resultado}")
+    print (f"A área do quadrado é {resultado}")
 
 def retangulo():
     base = float(input("Qual o valor da base do retângulo? "))
     altura = float(input("Qual o valor da altura do retângulo? "))
     resultado = base * altura
-    print (f"A área do seu retângulo é {resultado}")
+    print (f"A área do retângulo é {resultado}")
     
 def paralelogramo():
     base = float(input("Qual o valor da base do paralelogramo? "))
     altura = float(input("Qual o valor da altura do paralelogramo? "))
     resultado = base * altura
-    print (f"A área do seu paralelogramo é {resultado}")
+    print (f"A área do paralelogramo é {resultado}")
     
 def losango():
-    diagonal_maior = float(input("Qual o valor da diagonal maior? "))
-    diagonal_menor = float(input("Qual o valor da diagonal menor? "))
+    diagonal_maior = float(input("Qual o valor da diagonal maior do losango? "))
+    diagonal_menor = float(input("Qual o valor da diagonal menor do losango? "))
     resultado = (diagonal_maior * diagonal_menor) / 2
-    print (f"A área do seu losango é {resultado}")
+    print (f"A área do losango é {resultado}")
+    
+def trapezio():
+    base_maior = float(input("Qual o valor da base maior do trapézio? "))
+    base_menor = float(input("Qual o valor da base menor do trapézio? "))
+    altura = float(input("Qual o valor da altura do trapézio? "))
+    resultado = (base_maior + base_menor) * altura / 2
+    print (f"A área do trapézio é {resultado}")
 
 while True:
     print ("CALCULADORA DE ÁREAS DAS FORMAS GEOMÉTRICAS")
@@ -57,6 +64,8 @@ while True:
         paralelogramo()
     elif opcao == "6":
         losango()
+    elif opcao == "7":
+        trapezio()
     elif opcao == "0":
         print ("Saindo do sistema...")
         break
