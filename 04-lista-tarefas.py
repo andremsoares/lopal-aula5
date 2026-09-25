26589bf96027c339432408030d43cf27c58b92b5
ANDRÉ MATHEUS SOARES DA SILVA <26210897@dsn127m1246733.cfp127aluno.local>
2026-09-25 15:59:06 -0300

    adicionando tarefas

---
+++ 04-lista-tarefas.py
@@ -0,0 +1,18 @@
+# LISTA DE TAREFAS
+
+# 1 - Mostrar todas tarefas
+# 2 - Mostrar tarefas concluídas
+# 3 - Mostrar tarefas pendentes
+# 4 - Mostrar tarefas por prioridade
+# 5 - Cadastrar tarefa nova
+# 6 - Finalizar tarefa
+# 7 - Remover tarefa
+# 0 - Sair do sistema
+
+tarefas = [
+    {"Título": "Estudar", "Concluída": "Sim", "Prioridade": "Alta"},
+    {"Título": "Ler", "Concluída": "Não", "Prioridade": "Baixa"},
+    {"Título": "Jogar videogame", "Concluída": "Não", "Prioridade": "Baixa"},
+    {"Título": "Ir à academia", "Concluída": "Não", "Prioridade": "Alta"},
+    {"Título": "Lavar louça", "Concluída": "Sim", "Prioridade": "Baixa"}
+]
